31d76830feaf281046f0aa29eae32e29ada6b650
alexberg90 <ayazya@code.edu.az>
2023-05-14 15:14:59 +0400

    new update

---
+++ SQL Connection/SQL Connection/SQL Connection/DAL(data access layer)/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQL_Connection.Models;
+
+namespace SQL_Connection.DAL_data_access_layer_
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
