70143f1fab898be3d0b2515235fe4466cad5bf05
alexberg90 <ayazya@code.edu.az>
2023-05-14 08:13:32 +0400

    first commit

---
+++ ViewBagViewData/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewBagViewData.Models;
+
+namespace ViewBagViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //ViewData
+            var p = new Product
+            {
+                Id = 1,
+                Name = "Product1",
+                Price = 55
+            };
+            //ViewData["ProductName"]= p.Name;
+
+
+            //ViewData["Id"] = "Id";
+            //ViewData["Name"] = "Name";
+            //ViewData["Price"] = "Price";
+
+            //ViewBag
+
+            ViewBag.Id = 2;
+            ViewBag.Name = "Name";
+            ViewBag.Price = 25;
+
+            return View();
+        }
+    }
+}
